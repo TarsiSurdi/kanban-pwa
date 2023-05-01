f1635cf244bcb362401e60a83ba87490a2508a21
Tarcísio Surdi <tarsisurdi@gmail.com>
2023-04-30 23:34:07 -0300

    feat: added Icon component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./index.css";
+import { Icon } from "./components";
 
 function App() {
   return (
     <div className="h-screen flex justify-center items-center">
       <h1 className="text-center text-6xl">Hello World!</h1>
+      <Icon />
     </div>
   );
 }
---
+++ src/components/Icon.tsx
@@ -0,0 +1,5 @@
+import IconSVG from "@public/icons/icon.svg";
+
+export function Icon() {
+  return <img src={IconSVG} alt="Logo" />;
+}

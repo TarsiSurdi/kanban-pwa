6f18e85cdc1f0519b35d2392e93789c2a7775fc3
Tarcísio Surdi <tarsisurdi@gmail.com>
2023-04-30 22:40:01 -0300

    feat: added VitePWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ["icons/*.{png,ico,svg}"],
+      manifest: {
+        name: "Kanban",
+        short_name: "Kanban",
+        description: "Kanban PWA using Vite.js",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "icons/icon.png",
+            sizes: "256x256",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "icons/icon.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});

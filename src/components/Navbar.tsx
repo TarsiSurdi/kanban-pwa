cd6d8ec10b59cda7377166f5253f303d0ac3d0ca
Tarcísio Surdi <tarsisurdi@gmail.com>
2023-05-01 09:18:12 -0300

    fix: responsive NavBar icon size

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,16 +5,16 @@ import { Icon } from ".";
 export function Navbar() {
   return (
     <nav className="bg-neutral-400">
-      <div className="h-12 xs:h-16 px-2 xs:px-4 flex justify-between">
+      <div className="h-12 xs:h-16 px-2 xs:px-4 flex justify-between xs:text-base text-sm">
         <div className="flex items-center">
           <Icon className="w-6 xs:w-8" />
           <h2 className="dark:text-white text-md xs:text-xl font-semibold px-2 whitespace-nowrap">
             Platform Launch
           </h2>
-          <FaChevronDown className="fill-primary xs:text-md text-xs" />
+          <FaChevronDown className="fill-primary" />
         </div>
         <div className="flex items-center">
-          <button className="bg-primary sm:px-4 sm:py-2 px-1 py-1 rounded-2xl mr-2 xs:text-md text-xs flex justify-center items-center">
+          <button className="bg-primary sm:px-4 sm:py-2 px-1 py-1 rounded-2xl mr-2 flex justify-center items-center">
             <FaPlus className="fill-gray-200" />
           </button>
           <BsThreeDotsVertical className="dark:fill-neutral-100" />

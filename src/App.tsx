4f79dcfe14a00bb619d732a61a39d27a43f70a13
Tarcísio Surdi <tarsisurdi@gmail.com>
2023-04-30 23:03:54 -0300

    feat: added tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import "./index.css";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-center text-6xl">Hello World!</h1>
+    </div>
+  );
 }
 
 export default App;

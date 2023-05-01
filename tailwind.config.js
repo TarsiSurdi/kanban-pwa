494b2259444451bcb0f1703466809625984ecf21
Tarcísio Surdi <tarsisurdi@gmail.com>
2023-05-01 00:47:01 -0300

    feat: added custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,17 @@ export default {
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        neutral: {
+          50: "#B1ACC0",
+          100: "#8C8A99",
+          200: "#696773",
+          300: "#3F3F45",
+          400: "#2D2D33",
+          500: "#212126",
+        },
+        primary: "#645FC6",
+      },
     },
   },
   plugins: [],

d1abb8b2816875f372417b8b5e2b934a0c9c4cbf
Tarcísio Surdi <tarsisurdi@gmail.com>
2023-05-02 22:16:27 -0300

    fix: Footer position > md breakpoint

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { FaGithub } from "react-icons/fa";
 
 export function Footer() {
   return (
-    <footer className="group flex justify-center items-center hover:underline text-neutral-100 dark:hover:text-white dark:bg-neutral-400 max-h-max text-sm pt-3 pb-safe">
+    <footer className="group flex md:absolute md:bottom-0 md:right-0 md:mr-safe md:mb-safe md:p-2 md:rounded-2xl justify-center items-center hover:underline text-neutral-100 dark:hover:text-white dark:bg-neutral-400 max-h-max text-sm pt-3 pb-safe">
       <FaGithub className="dark:group-hover:text-white mr-2" />
       <a
         href="https://github.com/TarsiSurdi"

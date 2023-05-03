3a31f53911833218b52ab3c309163141ffd06697
Tarcísio Surdi <tarsisurdi@gmail.com>
2023-05-02 22:01:19 -0300

    fix: fixed Footer and <body> safe-area-inset

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { FaGithub } from "react-icons/fa";
 
 export function Footer() {
   return (
-    <footer className="group flex justify-center items-center hover:underline text-neutral-100 dark:hover:text-white dark:bg-neutral-400 max-h-max p-2 text-sm">
+    <footer className="group flex justify-center items-center hover:underline text-neutral-100 dark:hover:text-white dark:bg-neutral-400 max-h-max text-sm pt-3 pb-safe">
       <FaGithub className="dark:group-hover:text-white mr-2" />
       <a
         href="https://github.com/TarsiSurdi"
